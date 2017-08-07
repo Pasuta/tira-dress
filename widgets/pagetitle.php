7e675bfd00fb4fd323b8d7b6ebc033fa634caebd
vitaliy.pasiuta <vitaliy.pasiuta@stylelounge.de>
2017-08-07 13:51:54 +0200

    feat: add ceo text to title

--- widgets/pagetitle.php
+++ widgets/pagetitle.php
@@ -8,8 +8,9 @@ function widget_pagetitle($options)
     if (!is_array($title) && $title) $title = array($title);
     else if (!is_array($title) && !$title) $title = array();
     if (!$sitetitle) $sitetitle = SITE_NAME;
+    $ceo = "Свадебные платья в Киеве пошив и продажа. Приятные цены. каталог с фото. Салон \"ТИРА Шоурум\" в центре.";
 
-    array_push($title, $sitetitle);
+    array_push($title, $ceo);
 
     echo join(' / ', $title);
 }
