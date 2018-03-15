09dd913d79c490d80ebdb70bdd9fe92043d1e941
Vitaliy Pasiuta <vpas@ciklum.com>
2018-03-15 11:13:05 +0200

    seo updates

--- apps/Bonus/view/request.php
+++ apps/Bonus/view/request.php
@@ -21,63 +21,63 @@ extract($this->context);
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12" name="name" placeholder="Ваше имя">
-                        <div class="error left-align" id="err-email">Имя</div>
+                        <div class="error left-align">Имя</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12" name="weddingdate" placeholder="Дата свадьбы (25.12.2018)">
-                        <div class="error left-align" id="err-email">Дата свадьбы</div>
+                        <div class="error left-align">Дата свадьбы</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="dresscolor" placeholder="Цвет (белое, айвори, пудровое)">
-                        <div class="error left-align" id="err-email">Цвет платья</div>
+                        <div class="error left-align">Цвет платья</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="siluet" placeholder="Силует (узкое, средней пышности, очень пышное, прямое, другое)">
-                        <div class="error left-align" id="err-email">Силует платья</div>
+                        <div class="error left-align">Силует платья</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="dekor" placeholder="Декор (камни, бисер, кружево и т.д.)">
-                        <div class="error left-align" id="err-email">Декор платья</div>
+                        <div class="error left-align">Декор платья</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="size" placeholder="Размер (от 38 до 58)">
-                        <div class="error left-align" id="err-email">Размер</div>
+                        <div class="error left-align">Размер</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="fata" placeholder="Фата (длинная, короткая, средняя, не будет)">
-                        <div class="error left-align" id="err-email">Фата</div>
+                        <div class="error left-align">Фата</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="ready" placeholder="Пошив, готовая модель">
-                        <div class="error left-align" id="err-email">Готовое платье или пошив</div>
+                        <div class="error left-align">Готовое платье или пошив</div>
                     </div>
                 </div>
 
                 <div class="control-group">
                     <div class="controls">
                         <input type="text" class="span12"  name="phone" placeholder="+380 50 55 444 33">
-                        <div class="error left-align" id="err-email">Ваш телефон</div>
+                        <div class="error left-align">Ваш телефон</div>
                     </div>
                 </div>
 
